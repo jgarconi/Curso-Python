deaa1e3e9462ce076f2e08b8e5126823d3483c4d
Juliana Garconi <js.garconi@gmail.com>
2021-01-14 03:44:44 -0300

    Update class 17 file

--- secao02/fundamentos/formatacao.py
+++ secao02/fundamentos/formatacao.py
@@ -8,8 +8,8 @@
 # Exemplo 1: preencher números (ou texto) na direção desejada
 num_1 = 1
 print("Exemplo 1:")
-print(f"{num_1:0>9}")  # Esquerda
-print(f"{num_1:0<9}")  # Direita
+print(f"{num_1:0>9}")    # Esquerda
+print(f"{num_1:0<9}")    # Direita
 print(f"{num_1:0^9}\n")  # Centro
 
 # Exemplo 2: formatação de str
