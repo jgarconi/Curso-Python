57b408b2014e44a3a3d5703b5bf1e9b59bc307b5
jgarconi <68389491+jgarconi@users.noreply.github.com>
2021-01-17 18:55:59 -0300

    Update lesson 3 file

--- secao02/fundamentos/strings_e_aspas.py
+++ secao02/fundamentos/strings_e_aspas.py
@@ -4,15 +4,19 @@
 
     Exemplos de strings e uso de aspas em Pyhton
 """
-# Exemplo 1: \ como caracter de escape
+
+# Exemplo 1: uso da \ como caracter de escape
 print("Exemplo 1:")
 print("Essa \"string\" tem caracter de escape.\n")
 
-# Exemplo 2: \n como quebra de linha
+# Exemplo 2: alternância de aspas para evitar erros
 print("Exemplo 2:")
+print('Uma forma de evitar erros é alternar "aspas duplas" e simples.')
+
+# Exemplo 3: uso do \n como quebra de linha
+print("Exemplo 3:")
 print("Olá \n mundo!\n")
 
-# Exemplo 3: r para impedir ações dentro da str
-print("Exemplo 3:")
+# Exemplo 4: uso do r para impedir ações dentro da str
+print("Exemplo 4:")
 print(r"Olá \n mundo")
-
